a50aefee0af3e207ba867b7aef8399696c36b3e4
Saron Medeiros Philippi <saron.ph@gmail.com>
2023-02-22 20:39:19 -0300

    added aula 14

---
+++ aula-01/aula14/tests.ts
@@ -0,0 +1,9 @@
+var arr = ['S', 'a', 'r', 'o', 'n'];
+
+// var reduce = arr.reduceRight((acum, current, index, array) => {
+//   return acum + current;
+// });
+
+// console.log(reduce);
+
+console.log(arr.indexOf('a', 2));
