37f2f3cd68be9abad7b6e70d579a2f0c9b083d16
Saron Medeiros Philippi <saron.ph@gmail.com>
2023-03-02 22:13:08 -0300

    added aula 19

--- aula19 -  Regex3/desafio19.js
+++ aula19 -  Regex3/desafio19.js
@@ -7,125 +7,103 @@
 */
 
   /*
-Crie uma função chamada `cleanCPF`, que receba um CPF por parâmetro, e
-retorne esse CPF limpo (somente os números).
-Usando os CPFs abaixo, mostre no console que a limpeza funciona para todos
-eles! Use um console.log para cada CPF.
-- "049-214 3421-1"
-- "210.458.522-05"
-- "735 500 794 - 22"
-- "101.123-131x32"
+Alguns detalhes importantes que faltou falar na aula:
+1. O objeto RegExp() pode receber um segundo parâmetro, que são as flags:
+- var justNumbersAndLetters = new RegExp( '[\\da-z]', 'gi' );
+2. Como a expressão regular passada para o objeto RegExp() é uma string,
+ela pode ser concatenada para gerar uma regex em tempo de execução,
+diferente da regex literal, onde toda a regex precisa estar pronta antes
+da utilização.
+
+  /*
+- Usando o construtor de Regex, crie uma regex que case somente com números
+no início da string. O match precisa ser feito para todas as
+correspondências de qualquer string, não somente para a primeira, ainda que
+esta tenha muitas linhas, deve sempre casar com números no início de cada
+linha, independente de quantos caracteres de número estiverem juntos.
+- Atribua essa regex à uma variável chamada `justNumbersRegex` e mostre-a
+no console:
 */
-  console.log('Limpando CPFs:');
-  function cleanCPF(cpf) {
-    // return cpf.replace(/\D/g, ''); não números
-    return cpf.replace(/[^0-9]/g, '');
+  console.log('Regex para números usando o construtor:');
+  // ?
+  // var justNumbersRegex = new RegExp(/^\d+/gm);
+  var justNumbersRegex = new RegExp('^\\d+', 'gm');
+  /*
+Verifique se a regex acima casa com o texto na variável `text`, mostrando o
+resultado no console. O resultado deve ser:
+"[ '10', '50' ]"
+*/
+  var text =
+    '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
+  console.log('\nNúmeros no início da linha do texto:\n' + text, '\n');
+  // ?
+  console.log(text.match(justNumbersRegex));
+  /*
+- Crie uma regex que case com números no final de uma string. Atribua a
+regex à uma variável chamada `numbersAtTheEnd`.
+- A regex deve casar com todas as correspondências de qualquer string, ainda
+que esta tenha muitas linhas, deve sempre casar com números no fim de cada
+linha, independente de quantos caracteres de número estiverem juntos.
+Mostre a regex no console:
+*/
+  console.log('\nRegex para números somente no final das linhas:');
+  // ?
+  var numbersAtTheEnd = new RegExp('\\d+$', 'gm');
+  console.log(numbersAtTheEnd);
+
+  /*
+Verifique se a regex acima casa com o texto na variável `otherText`,
+mostrando o resultado no console.
+O resultado deve ser:
+"[ '12', '6' ]"
+*/
+  var otherText =
+    'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
+  console.log('\nNúmeros no final da linha:\n\n', otherText, '\n');
+  // ?
+  console.log(otherText.match(numbersAtTheEnd));
+
+  /*
+Vamos criar um método que vai testar se uma classe CSS existe em uma
+marcação HTML.
+- Primeiro, crie uma função chamada `hasClass`;
+- Essa função receberá dois parâmetros: o primeiro chamado `markup`, que
+será a marcação HTML testada, e o segundo `cssClass`, que será a classe que
+iremos testar;
+- A função deve retornar `true` se a classe existir na marcação e `false`
+caso contrário;
+- A marcação usada para testar deve ser a que está na variável `markup`
+abaixo;
+- Não altere a marcação na variável markup!
+- Faça o teste, mostrando no console o resultado para as seguintes classes:
+- "container", "text", "date", "excerpt" e "main".
+- O console deve exibir a frase:
+"[RESULTADO] para a classe [CLASSE]"
+- Ex. de resposta:
+"true para a classe container"
+- Teste uma classe por vez (um console.log por classe).
+- Lembrando que a função deve funcionar para qualquer marcação HTML e para
+qualquer classe que for testada. Os dados passados no exercício são somente
+para exemplificar.
+*/
+  var markup =
+    '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
+  console.log(
+    '\nQuais classes CSS existem na marcação abaixo?\n\n',
+    markup,
+    '\n'
+  );
+  // ?
+  function hasClass(markup, cssClass) {
+    var regex = new RegExp(
+      'class=["\'](?:[\\w\\s]+)?' + cssClass + '(?:[\\w\\s]+)?["\']'
+    );
+    return regex.test(markup);
   }
 
-  var cpfs = [
-    '049-214 3421-1',
-    '210.458.522-05',
-    '735 500 794 - 22',
-    '101.123-131x32',
-  ];
+  var classes = ['container', 'text', 'date', 'excerpt', 'main'];
 
-  cpfs.forEach((cpf) => {
-    return console.log(cleanCPF(cpf));
+  classes.forEach(function (cssClass) {
+    console.log(`${hasClass(markup, cssClass)} para a classe ${cssClass}`);
   });
-
-  /*
-Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
-Ex.: "999.999.999-99"
-Mostre o resultado no console.
-*/
-  console.log('\nFormatando CPFs corretamente:');
-  function formattedCpf(cpf) {
-    return cpf.replace(/(\d\d\d)(\d\d\d)(\d\d\d)(\d\d)/g, '$1.$2.$3-$4');
-    // return cpf.replace(
-    //   /(\d\d\d)(\d\d\d)(\d\d\d)(\d\d)/g,
-    //   (total, value1, value2, value3, value4) => {
-    //     return value1 + '.' + value2 + '.' + value3 + '-' + value4;
-    //   }
-    // );
-  }
-
-  cpfs.forEach((cpf) => {
-    return console.log(formattedCpf(cleanCPF(cpf)));
-  });
-
-  /*
-Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
-usando o mínimo de caracteres possíveis na regex.
-Para garantir que a regex funciona, teste-a usando o método match. Se houver
-o match, o método retorna um array com os matches. Caso contrário, ele
-retornará null.
-Mostre no console o resultado do match para a frase:
-"Os meses de janeiro, junho e julho começam com a letra j."
-O resultado deve ser:
-["junho", "julho"]
-*/
-  console.log(
-    '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":'
-  );
-  var stringMonth = 'Os meses de janeiro, junho e julho começam com a letra j.';
-  // console.log(stringMonth.match(/ju[nl]ho/g)); [nl] uma letra ou outra
-  console.log(stringMonth.match(/(junho|julho)/g));
-
-  /*
-Crie uma expressão regular que faça o match com a abertura de uma tag
-HTML qualquer.
-Ex.: "<div>", "<section>", "<blockquote>".
-Use o método match e faça o teste com a marcação abaixo:
-"<div><section><blockquote>Texto <img /></blockquote></section></div>"
-O resultado deve ser:
-["<div>", "<section>", "<blockquote>"]
-*/
-  console.log('\nMatch com a abertura de uma tag HTML:');
-  var tags =
-    '<div><section><blockquote>Texto <img /></blockquote></section></div>';
-
-  // console.log(tags.match(/<(\w+)>/g));
-  console.log(tags.match(/[<](\w+)[>]/g));
-  /*
-Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
-com a abertura e fechamento da tag.
-Ex.: "<div></div>", "<section></section>", "<blockquote></blockquote>".
-Use o método match e faça o teste com a marcação abaixo:
-"<div><ul><li></li><li></li><li><span></span></li></ul></div>"
-O resultado deve ser:
-["<li></li>", "<li></li>", "<span></span>"]
-*/
-  console.log('\nMatch com tags HTML vazias (abertura e fechamento da tag):');
-  var newTag = '<div><ul><li></li><li></li><li><span></span></li></ul></div>';
-  console.log(newTag.match(/[<](\w+)[>][<](\/\w+)[>]/g));
-
-  /*
-Vamos complicar um pouco agora :D
-
-Crie uma expressão regular que faça o match com um texto existente dentro de
-uma tag HTML. O texto deve ser capturado e substituído por:
-'O texto dentro da tag "[NOME DA TAG]" é "[TEXTO]"'
-
-Use a marcação abaixo para fazer o replace:
-"<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>"
-
-A marcação deve permanecer como está, somente o texto deve ser substituído.
-No replace, utilize quebras de linha para deixar uma tag por linha.
-
-O resultado deve ser esse:
-<h1>O texto dentro da tag "h1" é "Título da página"</h1>
-<p>O texto dentro da tag "p" é "Este é um parágrafo"</p>
-<footer>O texto dentro da tag "footer" é "Rodapé"</footer>
-
-Uma dica: faça o match aos poucos. Para facilitar o teste, use o site
-https://regex101.com/#javascript e verifique se as capturas estão
-corretas, para depois aplicar no código ;)
-*/
-  console.log('\nFazer replace dos textos das tags:');
-  var string =
-    '<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>';
-  var regex2 = /<(\w+)>([^<]+)<\/\w+>/g;
-  console.log(
-    string.replace(regex2, '<$1> O texto dentro da tag "$1" é "$2" </$1> \n')
-  );
 })();
--- aula19 -  Regex3/main.js
+++ aula19 -  Regex3/main.js
@@ -7,4 +7,23 @@
   // (<\w+>).+?(<\/\w+>) = ? não adiciona tudo, todas as tags foram divididas
   // ju(?:n|l)ho = match sem captura, exemplo junho e julho
   // <(\w+)>.+<\/(\1)> = 1 referencia da primeira captura, irá pegar uma mesma captura
+  // .match retorna todos os match de uma regex por ex, dentro do array
+  // .split pode ser usado regex para remover algo especifico
+  // .search busca a regex e retorna a posição ou -1 caso nao encontre, igual indexOf
+  // regex = new RegExp('\\d') forma de declarar regex, dessa forma precisa 2 barras para esquipar a string
+  // /\s/.test('saron') = true    = .test retorna boolean do regex
+
+  var name = 'saron123ar';
+  var regex = /\d/g;
+  var result;
+
+  // console.log('test', regex.exec(name));
+  // console.log('test', regex.exec(name));
+  // console.log('test', regex.exec(name));
+  // console.log('test', regex.exec(name));
+  // console.log('test', regex.exec(name));
+
+  while ((result = regex.exec(name) !== null)) {
+    console.log("Julio's bar", result);
+  }
 })();
