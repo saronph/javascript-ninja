1baed81175b7f70bed9a0fb6b18c1e3870997fc8
Saron Medeiros Philippi <saron.ph@gmail.com>
2023-03-17 21:37:55 -0300

    added aula32

---
+++ aula32/main.js
@@ -0,0 +1,36 @@
+(function (win, doc) {
+  'use strict';
+
+  console.log('null', typeof null);
+
+  // var ajax = new XMLHttpRequest();
+
+  // ajax.open('GET', 'http://localhost:3000/user/liz');
+  // ajax.send();
+  // ajax.addEventListener(
+  //   'readystatechange',
+  //   function (e) {
+  //     if (ajax.readyState === 4) {
+  //       console.log(ajax.responseText, ajax.status);
+  //     }
+  //   },
+  //   false
+  // );
+
+  // POST
+
+  // ajax.open('POST', 'http://localhost:3000/user');
+  // ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // ajax.send(
+  //   JSON.stringify({
+  //     name: 'liz',
+  //     age: 10,
+  //   })
+  // );
+
+  // ajax.onreadystatechange = function () {
+  //   if (ajax.readyState === 4) {
+  //     console.log('cadastrado');
+  //   }
+  // };
+})(window, document);
